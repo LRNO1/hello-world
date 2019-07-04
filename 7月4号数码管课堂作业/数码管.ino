68b00c9c68a9fc4c38041a39d64a04603436b29b
LRNO1 <1766470510@qq.com>
2019-07-04 22:32:42 +0800

    7月4号数码管作业

---
+++ 7月4号数码管课堂作业/数码管.ino
@@ -0,0 +1,51 @@
+void setup()
+{
+   pinMode(2,OUTPUT);
+   pinMode(3,OUTPUT);
+   pinMode(4,OUTPUT);
+   pinMode(5,OUTPUT);
+   Serial.begin(9600);
+}
+byte income=0;
+void loop()
+{
+  if(Serial.available()>0)
+  {
+    income=Serial.read();
+    income=income-'0';
+    delay(10);
+    if(income&0x1)
+    {
+      digitalWrite(2,HIGH);
+    }
+    else
+    {
+      digitalWrite(2,LOW);
+    }
+    if((income>>1)&0x1)
+    {
+      digitalWrite(5,HIGH);
+    }
+    else
+    {
+      digitalWrite(5,LOW);
+    }
+    if((income>>2)&0x1)
+    {
+      digitalWrite(4,HIGH);
+    }
+    else
+    {
+      digitalWrite(4,LOW);
+    }
+    if((income>>3)&0x1)
+    {
+      digitalWrite(3,HIGH);
+    }
+    else
+    {
+      digitalWrite(3,LOW);
+    }
+    delay(10);
+  }
+}
